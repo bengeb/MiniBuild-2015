aa393f871a19e767f3152f25a1d5138c96ee803b
ben <ben@gebhardts.net>
2015-11-07 14:33:47 -0500

    Added limit switch output as example

--- src/Robot.cpp
+++ src/Robot.cpp
@@ -1,4 +1,6 @@
 #include "WPILib.h"
+#include <sstream>
+
 
 /**
  * This is a demo program showing how to use Mecanum control with the RobotDrive class.
@@ -17,11 +19,14 @@ class Robot: public SampleRobot
 	RobotDrive robotDrive;	// robot drive system
 	Joystick stick;			// only joystick
 
+	DigitalInput limitSwitch;
+
 public:
 	Robot() :
 			robotDrive(frontLeftChannel, rearLeftChannel,
 					   frontRightChannel, rearRightChannel),	// these must be initialized in the same order
-			stick(joystickChannel)								// as they are declared above.
+			stick(joystickChannel),								// as they are declared above.
+			limitSwitch(0)
 	{
 		robotDrive.SetExpiration(0.1);
 		robotDrive.SetInvertedMotor(RobotDrive::kFrontLeftMotor, true);	// invert the left side motors
@@ -39,7 +44,9 @@ public:
         	// Use the joystick X axis for lateral movement, Y axis for forward movement, and Z axis for rotation.
         	// This sample does not use field-oriented drive, so the gyro input is set to zero.
 			robotDrive.MecanumDrive_Cartesian(stick.GetX(), stick.GetY(), stick.GetZ());
-
+			std::ostringstream limitData;
+			limitData<<"Hello world"<<limitSwitch.Get()?"1":"0";
+SmartDashboard::PutString("DB/String 3", limitData.str());
 			Wait(0.005); // wait 5ms to avoid hogging CPU cycles
 		}
 	}
